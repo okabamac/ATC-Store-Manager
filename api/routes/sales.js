d85aafc08eca25dc0a8d0ba376370523c9f1e4ae
Okaba Mac <markokaba99@gmail.com>
2018-10-25 11:07:58 +0100

    ft(store attendant can create a sale order) -given an attendant he can create sale order -he can see his order successfully posted -he receives an error if it is not correct

--- api/routes/sales.js
+++ api/routes/sales.js
@@ -20,10 +20,42 @@ router.get("/:saleId", (req, res, next) => {
         });
     }
 });
+//Handle Post Request for Sales
 router.post("/", (req, res, next) => {
-    res.status(200).json({
-        message: "Handling POST request to /products"
-    });
+    let reply = {};
+    const password = req.body.password;
+    if (password === "1234") {
+        if (
+            req.body.category &&
+            req.body.name &&
+            req.body.quantity &&
+            req.body.price
+        ) {
+            const myOrder = {
+                id: Date.now(),
+                product: {
+                    category: req.body.category,
+                    name: req.body.name,
+                    quantity: req.body.quantity,
+                    price: req.body.price,
+                }
+            };
+            reply = {
+                message: "Your product has been added successfully",
+                createdOrder: myOrder
+            };
+            salesDB.push(reply.createdProduct);
+        } else {
+            reply = {
+                message: "Missing fields not allowed"
+            };
+        }
+        res.status(200).json(reply);
+    } else {
+        res.status(200).json({
+            message: "Your password is wrong or you don't have access"
+        });
+    }
 });
 
 export default router;
