10bc5b6b2b629f4d29b5157f661e9749a981fea1
Okaba Mac <markokaba99@gmail.com>
2018-10-26 11:18:37 +0100

    ft: -added a markdown preview

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
-# ATC-Store-Manager
+## ATC-Store-Manager
 
 ATC Store Manager is an app that helps store owner manage sales and product inventory records. This application is meant for use in a single store.
 
 <img src="./ui/img/Capture.PNG" style="max-width: 1200px; height: 50vh">
+
+## Table of Content
+
+- [Getting Started](#Getting-Started)
+- [Technology Stack](#Technology-Stack)
+  \*Dependencies(#Dependencies)
+- [Installation and Usage](#Installation-and-Usage)
+- [Testing](#Testing)
+- [Feature](#Features)
+- [Routes for Express](#Routes-for-Express)
+- [Models](#Models)
+- [API Documentation](#API-Documentation)
+- [License](#License)
+- [FAQ](#FAQ)
--- api/controller/postProductController.js
+++ api/controller/postProductController.js
@@ -1,27 +1,51 @@
 import productDB from "../model/data/mockProductDb";
 export default class postProductController {
     static postProduct(req, res) {
-        if (!req.body.title) {
-            return res.status(400).send({
-                success: 'false',
-                message: 'title is required'
-            });
-        } else if (!req.body.description) {
-            return res.status(400).send({
-                success: 'false',
-                message: 'description is required'
+        const password = parseInt(req.params.password);
+        if (password === 123) {
+            if (!req.params.category) {
+                return res.send({
+                    message: "Category is required or invalid parameter"
+                });
+            }
+            if (!req.params.name) {
+                return res.send({
+                    message: "Name is required or invalid parameter"
+                });
+            }
+
+            if (!req.params.quantity) {
+                return res.send({
+                    message: "Quantity is required or invalid parameter"
+                });
+            }
+            if (!req.params.price) {
+                return res.send({
+                    message: "Price is required or invalid parameter"
+                });
+            }
+
+            if (req.params.category && req.params.name && req.params.quantity && req.params.price) {
+                const createdProduct = {
+                    id: Date.now(),
+                    product: {
+                        category: req.params.category,
+                        name: req.params.name,
+                        quantity: req.params.quantity,
+                        price: req.params.price
+                    }
+                };
+                productDB.unshift(createdProduct);
+                return res.send({
+                    message: "Product added successfully",
+                    createdProduct
+                });
+            }
+
+        } else {
+            return res.send({
+                message: "Access denied or invalid password"
             });
         }
-        const product = {
-            id: Date.now(),
-            title: req.body.title,
-            description: req.body.description
-        }
-        productDB.push(product);
-        return res.status(201).send({
-            success: 'true',
-            message: 'Product added successfully',
-            product
-        });
     }
 }
